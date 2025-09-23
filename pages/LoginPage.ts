bedcf5d7e79850b22899844825e40cc04b9aab4b
Adam <wudarczykadam@gmail.com>
2025-09-23 12:09:18 +0200

    feat(auth): add initial LoginPage object and test skeleton for ServiceNow

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -1,1 +1,18 @@
-TBD
+import { Page } from '@playwright/test';
+
+export const LoginPage = (page : Page) => {
+    const userInput = page.locator('#user_name');
+    const passInput = page.locator('#user_password');
+    const submitBtn = page.locator('#sysverb_login');
+    const errorMsg = page.locator('#output_messages .outputmsg_text');
+
+
+ return {
+     goto: async () => {
+         await page.goto('https://dev194623.service-now.com/login.do', {
+             waitUntil: 'domcontentloaded',
+     });
+   },
+
+ };
+};
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,1 +1,25 @@
-TBD
+import {test, expect} from '@playwright/test';
+import { LoginPage } from '@pages/LoginPage';
+
+const SN_USER = 'admin';
+const SN_PASS = 'OhluX%5cMJ2!';
+
+test.describe ('Login tests for ServiceNow', () => {
+    let login: ReturnType<typeof LoginPage>
+
+    test.beforeEach(async ({ page }) => {
+        login = LoginPage(page);
+        await login.goto();
+    });
+
+    test('TBD', async ({ page }) => {
+        //TBD
+    });
+
+    test('TBD', async ({ page }) => {
+        //TBD
+    });
+
+
+})
+
